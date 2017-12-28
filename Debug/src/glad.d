e21b3d35ec26b96af4f14cc11538cb18a2fd8186
majun <majun@mail.51vv.com>
2017-12-28 16:59:47 +0800

    delele obj

--- Debug/src/glad.d
+++
@@ -1,7 +0,0 @@
-src/glad.d: ../src/glad.c \
- /home/mj/disk/source/githup/glfw/deps/glad/glad.h \
- /home/mj/disk/source/githup/glfw/deps/KHR/khrplatform.h
-
-/home/mj/disk/source/githup/glfw/deps/glad/glad.h:
-
-/home/mj/disk/source/githup/glfw/deps/KHR/khrplatform.h:
