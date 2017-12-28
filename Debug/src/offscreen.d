2cc5e9cb64080d49d47ccab028ae74328dfcdbd4
majun <majun@mail.51vv.com>
2017-12-28 16:58:27 +0800

    init commit

---
+++ Debug/src/offscreen.d
@@ -0,0 +1,13 @@
+src/offscreen.d: ../src/offscreen.c \
+ /home/mj/disk/source/githup/glfw/deps/glad/glad.h \
+ /home/mj/disk/source/githup/glfw/deps/KHR/khrplatform.h \
+ /home/mj/disk/source/githup/glfw/deps/linmath.h \
+ /home/mj/disk/source/githup/glfw/deps/stb_image_write.h
+
+/home/mj/disk/source/githup/glfw/deps/glad/glad.h:
+
+/home/mj/disk/source/githup/glfw/deps/KHR/khrplatform.h:
+
+/home/mj/disk/source/githup/glfw/deps/linmath.h:
+
+/home/mj/disk/source/githup/glfw/deps/stb_image_write.h:
---
+++ Debug/src/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/glad.c \
+../src/offscreen.c 
+
+OBJS += \
+./src/glad.o \
+./src/offscreen.o 
+
+C_DEPS += \
+./src/glad.d \
+./src/offscreen.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -I/home/mj/majun/android-ndk-r9d/platforms/android-9/arch-arm/usr/include -I/home/mj/disk/source/githup/glfw/deps -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
